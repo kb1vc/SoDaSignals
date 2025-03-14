514cfeded6d095aab7041fcdbe4ee0d9be127cf0
Matthew Reilly <kb1vc@kb1vc.org>
2025-03-13 20:43:51 -0400

    debugging build

--- charliecloud/Ubuntu/build.sh
+++ charliecloud/Ubuntu/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export CH_IMAGE_STORAGE=`pwd`/images
-ch-image build -t ubuntu_sodasignals -f Dockerfile .
+ch-image build -t ubuntu_base -f DockerfileBase .
+ch-image build --rebuild  -t ubuntu_sodasignals -f Dockerfile .
